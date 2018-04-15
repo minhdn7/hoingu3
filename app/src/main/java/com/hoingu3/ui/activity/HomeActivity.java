a0e54710d345960b40bfcefe99743373139088b4
minhdn7 <minhdn231@gmail.com>
2018-04-15 16:16:31 +0700

    Initial commit

---
+++ app/src/main/java/com/hoingu3/ui/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.hoingu3.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.mkit.hoingu3.R;
+
+public class HomeActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
